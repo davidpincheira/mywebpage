43f89940f4bf2be81043dbbe4f774663d50e36a5
davidpincheira <dpincheira92@gmail.com>
2020-10-04 17:43:59 -0300

    add gatsby-plugin-verify-brave

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,12 @@ module.exports = {
     'gatsby-plugin-postcss',
     'gatsby-transformer-json',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: ['gatsby-source-filesystem', 'gatsby-plugin-verify-brave'],
       options: {
         name: 'data',
-        path: `${__dirname}/src/data`
+        path: `${__dirname}/src/data`,
+        token: '14a68ba80c18e5e1e1c3ca0d777698511e615595efb7cecf7825654e28dc3034',
+        domain: 'keen-davinci-854e86.netlify.app'
       }
     }
   ],
