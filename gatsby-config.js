0171fa72c843978e01f681109afaef21b9b7afc5
davidpincheira <dpincheira92@gmail.com>
2020-10-06 19:11:00 -0300

    add cv

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,10 @@ module.exports = {
     'gatsby-plugin-postcss',
     'gatsby-transformer-json',
     {
-      resolve: ['gatsby-source-filesystem', 'gatsby-plugin-verify-brave'],
+      resolve: 'gatsby-source-filesystem',
       options: {
         name: 'data',
-        path: `${__dirname}/src/data`,
-        token: '14a68ba80c18e5e1e1c3ca0d777698511e615595efb7cecf7825654e28dc3034',
-        domain: 'keen-davinci-854e86.netlify.app'
+        path: `${__dirname}/src/data`
       }
     }
   ],
--- src/components/codigofacilito.js
+++ src/components/codigofacilito.js
@@ -1,9 +1,10 @@
 import React from "react"
-var req = require.context("../img/iconos", false, /.*\.svg$/);
+import downloadFile from '../data/archivos/CV_DavidPincheira.pdf' 
+let req = require.context("../img/iconos", false, /.*\.svg$/);
 
 export default () => {
   return (   
-        <div className=" mx-auto p-16">
+        <div className="mx-auto p-16">
           <header className="text-center">
               <h2 className="text-3xl font-bold mb-8">Tecnologías utilizadas</h2>
           </header>
@@ -18,7 +19,15 @@ export default () => {
             })
           }
           </div>
-              
+          
+          <div className="mx-auto p-10">
+            <header className="text-center">
+                <a className="bg-gray-400 hover:bg-gray-500 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" href={downloadFile} download>
+                  <svg class="fill-current w-4 h-4 mr-2" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                  <span>Descargar CV</span>
+                </a>
+            </header>
+          </div>
         </div>
   );
 }
