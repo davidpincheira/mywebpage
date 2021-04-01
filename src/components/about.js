c156ac4851bb43bfe8622762886f3851a0478290
David Pincheira <dvt_1992@hotmail.com>
2021-04-01 18:55:58 -0300

    edit info

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ export default () => (
         <div className="flex-1">
           <h1 className="font-bold text-blue-800 text-4xl text-center lg:text-justify">Sobre mí</h1>
           <p className="text-blue-900 text-2xl font-medium tracking-wider lg:text-justify text-center max-w-4xl mt-10  ">
-            Soy desarrollador fullstack, recibido de Licenciado en Sistemas de la Información<span className="text-black text-opacity-50 ">Actualmente viviendo en Buenos Aires, Argentina. 
+            Soy desarrollador fullstack, recibido de Licenciado en Sistemas de la Información. <span className="text-black text-opacity-50 ">Actualmente viviendo en Buenos Aires, Argentina. 
             He trabajado con varios lenguajes de programacion pero actualmente me interesa mucho el mundo de Node y React, tambien react-native!
             </span>
           </p>
